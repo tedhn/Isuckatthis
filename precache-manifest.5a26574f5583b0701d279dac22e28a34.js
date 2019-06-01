443521a71bed0eb093013981e3b5c01f3d8af747
Ted <heinhtetnaing186@gmail.com>
2019-06-01 11:58:05 +0630

    Updates

---
+++ precache-manifest.5a26574f5583b0701d279dac22e28a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e886af8b779feb193df13b54d065c1",
+    "url": "/Isuckatthis/index.html"
+  },
+  {
+    "revision": "f8d62eb825702480244f",
+    "url": "/Isuckatthis/static/css/main.8e203c21.chunk.css"
+  },
+  {
+    "revision": "8568f50f7ceb80227548",
+    "url": "/Isuckatthis/static/js/2.d42b6f0b.chunk.js"
+  },
+  {
+    "revision": "f8d62eb825702480244f",
+    "url": "/Isuckatthis/static/js/main.8f34734e.chunk.js"
+  },
+  {
+    "revision": "e228ad12dc86e2aa902d",
+    "url": "/Isuckatthis/static/js/runtime~main.7e473429.js"
+  }
+]);
